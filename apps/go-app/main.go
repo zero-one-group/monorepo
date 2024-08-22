c69f1d88e44c38d10bc75794e0c3c8995da124a3
Aris Ripandi <riipandi@gmail.com>
2024-08-22 15:37:04 +0700

    fix: golang air not killed

--- apps/go-app/main.go
+++ apps/go-app/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 
 	"go-app/internal/routes"
 )
@@ -12,6 +17,7 @@ import (
 func main() {
 	e := echo.New()
 	e.HideBanner = true
+	e.Logger.SetLevel(log.INFO)
 
 	// Register logger middleware
 	// @see: https://echo.labstack.com/docs/middleware/logger
@@ -28,7 +34,21 @@ func main() {
 	// Register the routes
 	routes.SetupRoutes(e)
 
-	// TODO: read the listen address from the environment variable
-	listenAddr := "127.0.0.1:8080"
-	e.Logger.Fatal(e.Start(listenAddr))
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	// Start server
+	go func() {
+		// TODO: read the listen address from the environment variable
+		if err := e.Start("127.0.0.1:8080"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
