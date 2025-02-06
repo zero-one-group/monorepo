406bce0967617ee67018e63c5a3125e4ce0bfd15
Aris Ripandi <riipandi@gmail.com>
2025-02-07 06:52:44 +0700

    fix: shared ui template

--- template-shared-ui/src/components/aspect-ratio/aspect-ratio.css.ts
+++
@@ -1,7 +0,0 @@
-import { type VariantProps, tv } from 'tailwind-variants'
-
-export const aspectRatioStyles = tv({
-  base: 'relative',
-})
-
-export type AspectRatioVariants = VariantProps<typeof aspectRatioStyles>
