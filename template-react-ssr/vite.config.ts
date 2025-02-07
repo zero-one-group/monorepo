3f352a05139e398002071d963963280c533fe422
Aris Ripandi <riipandi@gmail.com>
2025-02-07 10:08:34 +0700

    feat: React SSR template

---
+++ template-react-ssr/vite.config.ts
@@ -0,0 +1,23 @@
+import { reactRouter } from '@react-router/dev/vite'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'pathe'
+import { isProduction } from 'std-env'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig(({ isSsrBuild }) => ({
+  envPrefix: 'VITE_' /* Prefix for environment variables */,
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: { port: {{ port_number }}, host: true },
+  publicDir: resolve('public'),
+  build: {
+    manifest: true,
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1024 * 4,
+    reportCompressedSize: false,
+    minify: isProduction,
+    rollupOptions: isSsrBuild ? { input: './server/app.ts' } : undefined,
+    terserOptions: { format: { comments: false } },
+  },
+  esbuild: { legalComments: 'inline' },
+}))
