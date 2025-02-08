6763ceee4ef736395eeab282696f89c3752286dc
Aris Ripandi <riipandi@gmail.com>
2025-02-08 23:45:00 +0700

    feat: Strapi template

---
+++ template-strapi/config/database.ts
@@ -0,0 +1,37 @@
+import { resolve } from 'pathe'
+
+export default ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite')
+
+  const connections = {
+    postgres: {
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        ssl: env.bool('DATABASE_SSL', false) && {
+          key: env('DATABASE_SSL_KEY', undefined),
+          cert: env('DATABASE_SSL_CERT', undefined),
+          ca: env('DATABASE_SSL_CA', undefined),
+          capath: env('DATABASE_SSL_CAPATH', undefined),
+          cipher: env('DATABASE_SSL_CIPHER', undefined),
+          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        },
+        schema: env('DATABASE_SCHEMA', 'public'),
+      },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    },
+    sqlite: {
+      connection: {
+        filename: resolve(env('DATABASE_FILENAME', '_data/data.db')),
+      },
+      useNullAsDefault: true,
+    },
+  }
+
+  return {
+    connection: {
+      client,
+      ...connections[client],
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  }
+}
---
+++ template-strapi/server.cjs
@@ -0,0 +1,12 @@
+#!/nodejs/bin/node
+
+/* @ref: https://docs.strapi.io/dev-docs/deployment */
+
+const strapi = require('@strapi/strapi')
+const path = require('node:path');
+
+strapi.createStrapi({
+  distDir: path.resolve(__dirname, './dist'),
+  serveAdminPanel: true,
+  autoReload: true,
+}).start()
