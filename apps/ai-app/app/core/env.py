d6ff74eaf8d1151b1944bdb380cce2f40a5320a9
Muhamad Iqbal <mirfmaster@gmail.com>
2025-03-25 15:18:35 +0700

    feat: generate basic app from template

---
+++ apps/ai-app/app/core/env.py
@@ -0,0 +1,31 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Env(BaseSettings):
+    """Application settings.
+
+    Loads environment variables with pydantic-settings.
+    """
+
+    ML_PREFIX_API: str
+    DEBUG: bool = False
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=True,
+    )
+
+
+@lru_cache
+def get_env() -> Env:
+    """Get cached application settings.
+
+    Uses lru_cache decorator to cache the settings instance.
+
+    Returns:
+        Settings: Application settings instance.
+    """
+    return Env()
---
+++ apps/ai-app/app/main.py
@@ -0,0 +1,25 @@
+from app.core.env import get_env
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+env = get_env()
+app = FastAPI(
+    title="Machine Learning API",
+    description="Machine Learning API",
+    version="0.1.0",
+    docs_url="/docs",
+    root_path=env.ML_PREFIX_API,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Machine Learning API"}
