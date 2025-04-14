a497cb16f11d34f8d7ec9b2d50510354f881b5f8
Muhamad Iqbal <mirfmaster@gmail.com>
2025-04-14 09:31:00 +0700

    feat: add db init

---
+++ template-fastapi-ai/app/core/database.py
@@ -0,0 +1,34 @@
+from collections.abc import Generator
+from typing import Annotated
+
+from app.core.env import get_env
+from app.core.logging import logger
+from fastapi import Depends
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import OperationalError, SQLAlchemyError
+from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+
+env = get_env()
+
+engine = create_engine(env.DATABASE_URL, echo=env.DEBUG)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# NOTE: Base class that will be inherited across model
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        # closing and returning to connection pool
+        # ref: https://stackoverflow.com/a/8705750
+        db.close()
+
+
+# NOTE: Type alias for database session dependency
+DepDB = Annotated[Session, Depends(get_db)]
