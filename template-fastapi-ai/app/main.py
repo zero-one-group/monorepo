3c6fabcc0bb6e4ab8cdab2c9dd5a393366c7c97a
Muhamad Iqbal <mirfmaster@gmail.com>
2025-04-23 10:23:51 +0700

    ref: rename from system route to root router

--- template-fastapi-ai/app/main.py
+++ template-fastapi-ai/app/main.py
@@ -1,7 +1,7 @@
 from app.core.env import get_env
 from app.core.logging import RequestIdMiddleware, logger
-from app.router.main import router as system_router
 from app.router.openai import router as openai_router
+from app.router.root import router as root_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -30,5 +30,5 @@ app.add_middleware(
 )
 app.add_middleware(RequestIdMiddleware)
 
-app.include_router(system_router)
+app.include_router(root_router)
 app.include_router(openai_router)
