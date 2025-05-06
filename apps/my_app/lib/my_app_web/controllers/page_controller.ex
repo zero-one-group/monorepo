9e1a2d9fc15ceba97a4cd8d8a9a6b0520e246387
Aris Ripandi <riipandi@gmail.com>
2025-05-06 20:13:36 +0700

    chore: remove unused generated example apps

--- apps/my_app/lib/my_app_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule MyAppWeb.PageController do
-  use MyAppWeb, :controller
-
-  def home(conn, _params) do
-    render(conn, :home)
-  end
-end
--- apps/react-frontend/app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, route } from '@react-router/dev/routes'
-
-export default [
-  index('routes/home/page.tsx'),
-  route('login', 'routes/auth/login/page.tsx'),
-] satisfies RouteConfig
