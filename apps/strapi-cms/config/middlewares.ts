f78a1fbcccc77353cccc616305887bbf631cfbd7
Aris Ripandi <riipandi@gmail.com>
2025-05-07 16:13:56 +0700

    fix: Strapi S3 media library thumbnail not loaded

--- apps/strapi-cms/config/middlewares.ts
+++ apps/strapi-cms/config/middlewares.ts
@@ -1,17 +1,37 @@
-export default ({ env }) => [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      headers: '*',
-      origin: [env('STRAPI_ALLOWED_ORIGIN', '*')],
+export default ({ env }) => {
+  const awsBucket = env('AWS_BUCKET')
+  const awsRegion = env('AWS_REGION', 'ap-southeast-1')
+  const s3AssetsUrl = awsBucket ? `${awsBucket}.s3.${awsRegion}.amazonaws.com` : undefined
+
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'script-src': ["'self'", "'unsafe-inline'"],
+            'img-src': ["'self'", 'data:', 'blob:', s3AssetsUrl],
+            'media-src': ["'self'", 'data:', 'blob:', s3AssetsUrl],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
     },
-  },
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-]
+    {
+      name: 'strapi::cors',
+      config: {
+        headers: '*',
+        origin: [env('STRAPI_ALLOWED_ORIGIN', '*')],
+      },
+    },
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ]
+}
--- template-strapi/config/plugins.ts
+++ template-strapi/config/plugins.ts
@@ -1,48 +1,52 @@
-import { isDevelopment } from 'std-env'
+export default ({ env }) => {
+  const awsBucket = env('AWS_BUCKET')
+  const awsRegion = env('AWS_REGION', 'ap-southeast-1')
+  const s3AssetsUrl = awsBucket ? `${awsBucket}.s3.${awsRegion}.amazonaws.com` : ''
 
-export default ({ env }) => ({
-  email: {
-    config: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: env('SMTP_HOST', 'localhost'),
-        port: env('SMTP_PORT', 1025),
-        auth: {
-          user: env('SMTP_USERNAME'),
-          pass: env('SMTP_PASSWORD'),
+  return {
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('SMTP_HOST', 'localhost'),
+          port: env('SMTP_PORT', 1025),
+          auth: {
+            user: env('SMTP_USERNAME'),
+            pass: env('SMTP_PASSWORD'),
+          },
+          ignoreTLS: process.env.NODE_ENV === 'development',
         },
-        ignoreTLS: isDevelopment,
-      },
-      settings: {
-        defaultFrom: env('SMTP_EMAIL_FROM', 'admin@example.com'),
-        defaultReplyTo: env('SMTP_REPLY_TO', 'admin@example.com'),
-      },
-    },
-  },
-  upload: {
-    config: {
-      provider: 'aws-s3',
-      providerOptions: {
-        baseUrl: env('CDN_URL'),
-        rootPath: env('CDN_ROOT_PATH'),
-        s3Options: {
-          credentials: {
-            accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            secretAccessKey: env('AWS_ACCESS_SECRET'),
-          },
-          region: env('AWS_REGION'),
-          params: {
-            ACL: env('AWS_ACL', 'public-read'),
-            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-            Bucket: env('AWS_BUCKET'),
-          },
+        settings: {
+          defaultFrom: env('SMTP_EMAIL_FROM', 'admin@example.com'),
+          defaultReplyTo: env('SMTP_REPLY_TO', 'admin@example.com'),
         },
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
+    },
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          baseUrl: s3AssetsUrl,
+          rootPath: env('S3_PATH_PREFIX'),
+          s3Options: {
+            credentials: {
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_ACCESS_SECRET'),
+            },
+            region: awsRegion,
+            params: {
+              ACL: env('AWS_ACL', 'public-read'),
+              signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+              Bucket: env('AWS_BUCKET'),
+            },
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
       },
     },
-  },
-})
+  }
+}
