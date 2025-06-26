4f81464a778aecf53087a7f5b34a0a805ec51a6e
Muhamad Iqbal <mirfmaster@gmail.com>
2025-06-26 10:33:41 +0700

    feat: implement tracing

--- apps/ai-app/app/services/greeting.py
+++ apps/ai-app/app/services/greeting.py
@@ -1,6 +1,9 @@
 from app.core.logging import get_logger
 from app.core.response import ErrorResponse, SuccessResponse, success_response
+from app.core.trace import get_tracer
 from app.repository.openai.dependency import DepGreetingRepoOpenAI
+
+tracer = get_tracer("service.greetings")
 
 
 class GreetingService:
@@ -10,6 +13,7 @@ class GreetingService:
         self.__repo = repo
 
     async def greetings(self) -> SuccessResponse[dict] | ErrorResponse:
-        self.__log.info("Service layer log", extra={"layer": "service"})
-        greetings = await self.__repo.greetings()
-        return success_response(data=greetings)
+        with tracer.start_as_current_span("service.greetings") as span:
+            self.__log.info("Service layer log", extra={"layer": "service"})
+            greetings = await self.__repo.greetings()
+            return success_response(data=greetings)
