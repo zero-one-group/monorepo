b2110024e676234f98e2cffd584927a4b692f7ae
Aris Ripandi <riipandi@gmail.com>
2025-09-10 00:39:00 +0700

    feat: add testcontainer utils

--- apps/go-modular/go.mod
+++ apps/go-modular/go.mod
@@ -7,33 +7,91 @@ toolchain go1.25.1
 require (
 	github.com/jackc/pgx/v5 v5.7.6
 	github.com/pressly/goose/v3 v3.25.0
+	github.com/redis/go-redis/v9 v9.13.0
 	github.com/spf13/cobra v1.10.1
+	github.com/stretchr/testify v1.11.1
 	github.com/swaggo/swag v1.16.6
+	github.com/testcontainers/testcontainers-go v0.38.0
 )
 
 require (
+	dario.cat/mergo v1.0.1 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/containerd/errdefs v1.0.0 // indirect
+	github.com/containerd/errdefs/pkg v0.3.0 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/containerd/platforms v0.2.1 // indirect
+	github.com/cpuguy83/dockercfg v0.3.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/docker v28.2.2+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/ebitengine/purego v0.8.4 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.19.6 // indirect
 	github.com/go-openapi/spec v0.20.4 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/magiconair/properties v1.8.10 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/go-archive v0.1.0 // indirect
+	github.com/moby/patternmatcher v0.6.0 // indirect
+	github.com/moby/sys/sequential v0.6.0 // indirect
+	github.com/moby/sys/user v0.4.0 // indirect
+	github.com/moby/sys/userns v0.1.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
+	github.com/shirou/gopsutil/v4 v4.25.5 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
+	go.opentelemetry.io/otel v1.38.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.38.0 // indirect
+	go.opentelemetry.io/otel/metric v1.38.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.38.0 // indirect
+	go.opentelemetry.io/otel/trace v1.38.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.8.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/mod v0.27.0 // indirect
-	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
 	golang.org/x/tools v0.35.0 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ apps/go-modular/pkg/testutils/testcontainer.go
@@ -0,0 +1,169 @@
+package testutils
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+// TestEnv groups all test dependencies for integration tests.
+type TestEnv struct {
+	T           *testing.T
+	Ctx         context.Context
+	PGPool      *pgxpool.Pool
+	RedisClient *redis.Client
+	PGURL       string
+	RedisAddr   string
+
+	postgresC testcontainers.Container
+	redisC    testcontainers.Container
+}
+
+// NewTestEnv returns a new TestEnv.
+func NewTestEnv(t *testing.T) *TestEnv {
+	return &TestEnv{
+		T:   t,
+		Ctx: context.Background(),
+	}
+}
+
+// SetupPostgres starts a Postgres container and initializes the PGPool.
+func (te *TestEnv) SetupPostgres() (*pgxpool.Pool, string, error) {
+	t := te.T
+	ctx := te.Ctx
+
+	var err error
+	te.postgresC, err = testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:        "postgres:18rc1-alpine",
+			ExposedPorts: []string{"5432/tcp"},
+			Env: map[string]string{
+				"POSTGRES_PASSWORD": "testpass",
+				"POSTGRES_USER":     "testuser",
+				"POSTGRES_DB":       "testdb",
+			},
+			WaitingFor: wait.ForListeningPort("5432/tcp").WithStartupTimeout(120 * time.Second),
+		},
+		Started: true,
+	})
+	if err != nil {
+		return nil, "", fmt.Errorf("start postgres container: %w", err)
+	}
+	t.Cleanup(func() { _ = te.postgresC.Terminate(ctx) })
+
+	pgEndpoint, err := te.postgresC.Endpoint(ctx, "")
+	if err != nil {
+		return nil, "", fmt.Errorf("get postgres endpoint: %w", err)
+	}
+	pgHost, pgPortStr, err := net.SplitHostPort(strings.TrimPrefix(pgEndpoint, "tcp://"))
+	if err != nil {
+		return nil, "", fmt.Errorf("parse postgres endpoint: %w", err)
+	}
+	pgURL := fmt.Sprintf("postgres://testuser:testpass@%s:%s/testdb?sslmode=disable", pgHost, pgPortStr)
+
+	poolConfig, err := pgxpool.ParseConfig(pgURL)
+	if err != nil {
+		return nil, "", fmt.Errorf("parse pg config: %w", err)
+	}
+	poolConfig.MaxConns = 5
+	poolConfig.MinConns = 1
+	poolConfig.MaxConnLifetime = time.Hour
+	poolConfig.MaxConnIdleTime = 30 * time.Minute
+	poolConfig.ConnConfig.RuntimeParams["search_path"] = "public,reference,scheduler"
+	poolConfig.ConnConfig.RuntimeParams["timezone"] = "UTC"
+
+	pgPool, err := pgxpool.NewWithConfig(ctx, poolConfig)
+	if err != nil {
+		return nil, "", fmt.Errorf("create pg pool: %w", err)
+	}
+	t.Cleanup(func() { pgPool.Close() })
+
+	te.PGPool = pgPool
+	te.PGURL = pgURL
+	return pgPool, pgURL, nil
+}
+
+// SetupRedis starts a Redis container and initializes the RedisClient.
+func (te *TestEnv) SetupRedis() (*redis.Client, string, error) {
+	t := te.T
+	ctx := te.Ctx
+
+	var err error
+	te.redisC, err = testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:        "redis:8-alpine",
+			ExposedPorts: []string{"6379/tcp"},
+			WaitingFor:   wait.ForListeningPort("6379/tcp").WithStartupTimeout(60 * time.Second),
+		},
+		Started: true,
+	})
+	if err != nil {
+		return nil, "", fmt.Errorf("start redis container: %w", err)
+	}
+	t.Cleanup(func() { _ = te.redisC.Terminate(ctx) })
+
+	redisEndpoint, err := te.redisC.Endpoint(ctx, "")
+	if err != nil {
+		return nil, "", fmt.Errorf("get redis endpoint: %w", err)
+	}
+	redisHost, redisPortStr, err := net.SplitHostPort(strings.TrimPrefix(redisEndpoint, "tcp://"))
+	if err != nil {
+		return nil, "", fmt.Errorf("parse redis endpoint: %w", err)
+	}
+	redisAddr := fmt.Sprintf("%s:%s", redisHost, redisPortStr)
+
+	redisClient := redis.NewClient(&redis.Options{Addr: redisAddr, DB: 15})
+	if err := redisClient.Ping(ctx).Err(); err != nil {
+		return nil, "", fmt.Errorf("redis ping: %w", err)
+	}
+	t.Cleanup(func() {
+		_ = redisClient.Close()
+	})
+
+	te.RedisClient = redisClient
+	te.RedisAddr = redisAddr
+	return redisClient, redisAddr, nil
+}
+
+// SetupConfig sets the environment variables for the test configuration.
+func (te *TestEnv) SetupConfig() {
+	if te.PGURL != "" {
+		if err := os.Setenv("DB_POSTGRES_URL", te.PGURL); err != nil {
+			te.T.Logf("Failed to set DB_POSTGRES_URL: %v", err)
+		}
+	}
+	if te.RedisAddr != "" {
+		if err := os.Setenv("DB_REDIS_URL", fmt.Sprintf("redis://%s", te.RedisAddr)); err != nil {
+			te.T.Logf("Failed to set DB_REDIS_URL: %v", err)
+		}
+		if err := os.Setenv("DB_REDIS_ENABLED", "true"); err != nil {
+			te.T.Logf("Failed to set DB_REDIS_ENABLED: %v", err)
+		}
+	}
+	if err := os.Setenv("LOG_LEVEL", "debug"); err != nil {
+		te.T.Logf("Failed to set LOG_LEVEL: %v", err)
+	}
+}
+
+// RunAppMigrations runs the database migrations for the application.
+func (te *TestEnv) RunAppMigrations() {
+	cmd := exec.Command("go", "run", "-tags", "debug", "./cmd/", "migrate:up")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = os.Environ()
+
+	te.T.Log("Running test database migrations")
+	err := cmd.Run()
+	require.NoError(te.T, err, "failed to run test database migrations")
+}
