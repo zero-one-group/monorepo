9dec331aa9ac6efc561099416c15a7ed343859ad
Aris Ripandi <riipandi@gmail.com>
2025-09-17 02:22:33 +0700

    fix: overhaul Hugo styles

--- docsite/assets/js/color-preference.js
+++ docsite/assets/js/color-preference.js
@@ -1,36 +1,37 @@
 const lsKeyColorPreference = 'color-preference'
 
 const getColorPreference = () => {
-    let lastUsedColorPreference = localStorage.getItem(lsKeyColorPreference)
-    if (lastUsedColorPreference !== null)
-        return lastUsedColorPreference
-    else
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  let lastUsedColorPreference = localStorage.getItem(lsKeyColorPreference)
+  if (lastUsedColorPreference !== null) return lastUsedColorPreference
+  else return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
 }
 
 let colorPreference = getColorPreference()
 localStorage.setItem(lsKeyColorPreference, colorPreference)
 document.firstElementChild.setAttribute('data-color', colorPreference)
 
-const colorPreferenceButton = document.querySelector('#content-wrapper > footer > div:last-child > button:last-child')
+const colorPreferenceButton = document.querySelector(
+  '#content-wrapper > footer > div:last-child > button:last-child'
+)
 if (colorPreferenceButton) {
-    colorPreferenceButton.addEventListener('click', function () {
-        if (colorPreference == 'dark') {
-            colorPreference = 'light'
-        } else {
-            colorPreference = 'dark'
-        }
-        setColorPreference()
-    });
+  colorPreferenceButton.addEventListener('click', function () {
+    if (colorPreference == 'dark') {
+      colorPreference = 'light'
+    } else {
+      colorPreference = 'dark'
+    }
+    setColorPreference()
+  })
 }
 
 const setColorPreference = () => {
-    localStorage.setItem(lsKeyColorPreference, colorPreference)
-    document.firstElementChild.setAttribute('data-color', colorPreference)
+  localStorage.setItem(lsKeyColorPreference, colorPreference)
+  document.firstElementChild.setAttribute('data-color', colorPreference)
 }
 
-window.matchMedia('(prefers-color-scheme: dark)')
-    .addEventListener('change', ({matches: isDark}) => {
-        colorPreference = isDark ? 'dark' : 'light'
-        setColorPreference()
-    })
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', ({ matches: isDark }) => {
+    colorPreference = isDark ? 'dark' : 'light'
+    setColorPreference()
+  })
--- docsite/assets/js/off-canvas.js
+++ docsite/assets/js/off-canvas.js
@@ -1,4 +1,4 @@
-const body = document.body;
+const body = document.body
 const bodyModelOuter = document.querySelector('#body-model-outer')
 
 const asideWrapper = document.querySelector('#aside-wrapper')
@@ -6,23 +6,23 @@ const asideWrapperOpenButton = document.querySelector('main > article > nav > bu
 const asideWrapperCloseButton = document.querySelector('#aside-wrapper > aside > div .btn')
 
 if (asideWrapper && asideWrapperOpenButton) {
-    asideWrapperOpenButton.addEventListener('click', function () {
-        body.classList.add('model-open')
-        bodyModelOuter.style.display = 'block'
-        asideWrapper.classList.add('open')
+  asideWrapperOpenButton.addEventListener('click', function () {
+    body.classList.add('model-open')
+    bodyModelOuter.style.display = 'block'
+    asideWrapper.classList.add('open')
 
-        asideWrapperCloseButton.addEventListener('click', function () {
-            body.classList.remove('model-open')
-            bodyModelOuter.style.display = 'none'
-            asideWrapper.classList.remove('open')
-        });
+    asideWrapperCloseButton.addEventListener('click', function () {
+      body.classList.remove('model-open')
+      bodyModelOuter.style.display = 'none'
+      asideWrapper.classList.remove('open')
+    })
 
-        bodyModelOuter.addEventListener('click', function () {
-            body.classList.remove('model-open')
-            bodyModelOuter.style.display = 'none'
-            asideWrapper.classList.remove('open')
-        });
-    });
+    bodyModelOuter.addEventListener('click', function () {
+      body.classList.remove('model-open')
+      bodyModelOuter.style.display = 'none'
+      asideWrapper.classList.remove('open')
+    })
+  })
 }
 
 const mainAside = document.querySelector('main > aside')
@@ -30,52 +30,51 @@ const mainAsideOpenButton = document.querySelector('main > article > nav > butto
 const mainAsideCloseButton = document.querySelector('main > aside > div > .btn')
 
 if (mainAside && mainAsideOpenButton) {
-    mainAsideOpenButton.addEventListener('click', function () {
-        body.classList.add('model-open')
-        bodyModelOuter.style.display = 'block'
-        mainAside.classList.add('open')
+  mainAsideOpenButton.addEventListener('click', function () {
+    body.classList.add('model-open')
+    bodyModelOuter.style.display = 'block'
+    mainAside.classList.add('open')
 
-        mainAsideCloseButton.addEventListener('click', function () {
-            body.classList.remove('model-open')
-            bodyModelOuter.style.display = 'none'
-            mainAside.classList.remove('open')
-        });
+    mainAsideCloseButton.addEventListener('click', function () {
+      body.classList.remove('model-open')
+      bodyModelOuter.style.display = 'none'
+      mainAside.classList.remove('open')
+    })
 
-        bodyModelOuter.addEventListener('click', function () {
-            body.classList.remove('model-open')
-            bodyModelOuter.style.display = 'none'
-            mainAside.classList.remove('open')
-        });
-    });
+    bodyModelOuter.addEventListener('click', function () {
+      body.classList.remove('model-open')
+      bodyModelOuter.style.display = 'none'
+      mainAside.classList.remove('open')
+    })
+  })
 }
 
 window.addEventListener('resize', function (event) {
-    if (body.classList.contains('model-open')) {
-        body.classList.remove('model-open')
-        bodyModelOuter.style.display = 'none'
+  if (body.classList.contains('model-open')) {
+    body.classList.remove('model-open')
+    bodyModelOuter.style.display = 'none'
 
-        if (mainAside.classList.contains('open')) {
-            mainAside.classList.remove('open')
-        }
-
-        if (asideWrapper.classList.contains('open')) {
-            asideWrapper.classList.remove('open')
-        }
+    if (mainAside.classList.contains('open')) {
+      mainAside.classList.remove('open')
     }
-});
+
+    if (asideWrapper.classList.contains('open')) {
+      asideWrapper.classList.remove('open')
+    }
+  }
+})
 
 const asideWrapperSiteLogo = document.querySelector('#aside-wrapper .site-logo')
 const asideWrapperAsideNav = document.querySelector('#aside-wrapper > aside > nav')
 const adjustAsideWrapperAsideNavHeight = function () {
-    if (window.innerWidth > 1280) {
-        asideWrapperAsideNav.style.height = `${window.innerHeight - 1 - asideWrapperSiteLogo.getBoundingClientRect().height}px`
-    } else {
-        asideWrapperAsideNav.style.height = ''
-    }
+  if (window.innerWidth > 1280) {
+    asideWrapperAsideNav.style.height = `${window.innerHeight - 1 - asideWrapperSiteLogo.getBoundingClientRect().height}px`
+  } else {
+    asideWrapperAsideNav.style.height = ''
+  }
 }
 
 adjustAsideWrapperAsideNavHeight()
 window.addEventListener('resize', function (event) {
-    adjustAsideWrapperAsideNavHeight();
+  adjustAsideWrapperAsideNavHeight()
 })
-
